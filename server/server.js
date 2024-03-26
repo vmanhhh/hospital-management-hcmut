30ebcaba0cdabed3e5982c948298b622323b9a50
vmanhhhh <tv.manh0706@gmail.com>
2024-03-26 23:20:45 +0700

    Init

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const uri = "mongodb+srv://admin99:3QWRrIoCgUwAMH5y@project-hospital-hcmut.xhbaroi.mongodb.net/?retryWrites=true&w=majority&appName=project-hospital-hcmut";
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connect successfully");
+    } catch (e) {
+        console.error(e);
+    }
+}
+connect();
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
