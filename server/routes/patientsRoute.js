4a6db7b992d36a3b087f6b96092c4bc96d97e440
vmanhhhh <tv.manh0706@gmail.com>
2024-04-10 23:41:21 +0700

    Update patientsRoute.js

--- server/routes/patientsRoute.js
+++ server/routes/patientsRoute.js
@@ -1,66 +1,19 @@
 import express from "express";
 import { Patient } from '../models/patientModel.js';
-import mongoose from "mongoose";
+import { 
+    addNewPatient,
+    getAllPatients,
+    findPatientById,
 
+} from '../controllers/patientsController.js'
 const router = express.Router();
 
 // Route for adding a new patient
-router.post('/new', async (req, res) => {
-    try {
-        // Check if data included required info
-        if (
-            !req.body.lastName ||
-            !req.body.firstName ||
-            !req.body.dob ||
-            !req.body.gender ||
-            !req.body.citizenId
-        ) {
-            return res.status(400).send({ message: 'Fill in the required fields' })
-        }
-        const newPatient = {
-            _id: new mongoose.Types.ObjectId(),
-            lastName: req.body.lastName,
-            firstName: req.body.firstName,
-            dob: req.body.dob,
-            gender: req.body.gender,
-            citizenId: req.body.citizenId,
-            address: req.body.address,
-            contactInfo: req.body.contactInfo,
-            emergencyContact: req.body.emergencyContact,
-            medicalRecord: req.body.medicalRecord,
-            allergies: req.body.allergies
-        }
-        const patient = await Patient.create(newPatient);
-        return res.status(201).send(patient);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message });
-    }
-});
+router.post('/', addNewPatient);
 
-router.get('/all', async (req, res) => {
-    try {
-        const patients = await Patient.find({});
-        return res.status(200).json({
-            count: patients.length,
-            data: patients,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message });
-    }
-});
+router.get('/', getAllPatients);
 
-router.get('/search/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const patients = await Patient.findById(id);
-        return res.status(200).json(patients);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message });
-    }
-});
+router.get('/:id', findPatientById);
 
 router.delete('/delete/:id', async (req, res) => {
     try {
